228d43d97b9c335baa547e304b19460a1b1aef15
Socialz <socialz.fin@gmail.com>
2014-03-30 14:39:58 +0300

    account: addendum selection menu

--- resources/account/cegui/client/selection.lua
+++ resources/account/cegui/client/selection.lua
@@ -4,6 +4,7 @@ cegui.windows.selection.window = { }
 cegui.windows.selection.label = { }
 cegui.windows.selection.edit = { }
 cegui.windows.selection.gridlist = { }
+cegui.windows.selection.gridlist_c = { }
 cegui.windows.selection.button = { }
 
 local window_width, window_height = 320, 320
@@ -22,11 +23,24 @@ function showSelectionWindow( )
 	
 	cegui.windows.selection.label.header = guiCreateLabel( 30, 40, window_width - 60, 15, "Select a character", false, cegui.windows.selection.window.base )
 	cegui.windows.selection.gridlist.characters = guiCreateGridList( 30, 65, window_width - 60, window_height - 180, false, cegui.windows.selection.window.base )
-	guiGridListAddColumn( cegui.windows.selection.gridlist.characters, "Name", 0.475 )
-	guiGridListAddColumn( cegui.windows.selection.gridlist.characters, "Last Used", 0.375 )
+	cegui.windows.selection.gridlist_c.name = guiGridListAddColumn( cegui.windows.selection.gridlist.characters, "Name", 0.475 )
+	cegui.windows.selection.gridlist_c.last_used = guiGridListAddColumn( cegui.windows.selection.gridlist.characters, "Last Used", 0.375 )
 	
 	cegui.windows.selection.button.select = guiCreateButton( 30, 225, window_width - 60, 30, "Select this character", false, cegui.windows.selection.window.base )
 	cegui.windows.selection.button.create = guiCreateButton( 30, 270, window_width - 60, 30, "Create a character", false, cegui.windows.selection.window.base )
 	
+	triggerServerEvent( getResourceName( resource ) .. ":cegui:characters", localPlayer )
+	
 	showCursor( true, true )
 end
+
+addEvent( getResourceName( resource ) .. ":cegui:characters", true )
+addEventHandler( getResourceName( resource ) .. ":cegui:characters", root,
+	function( _characters )
+		for _,character in pairs( _characters ) do
+			local row = guiGridListAddRow( cegui.windows.selection.gridlist.characters )
+			guiGridListSetItemText( cegui.windows.selection.gridlist.characters, row, cegui.windows.selection.gridlist_c.name, character.name:gsub( "_", " " ), false, false )
+			guiGridListSetItemText( cegui.windows.selection.gridlist.characters, row, cegui.windows.selection.gridlist_c.last_used, ( math.abs( character.last_login ) == 0 and "Today" or math.abs( character.last_login ) .. " day" .. ( math.abs( character.last_login ) == 1 and "" or "s" ) .. " ago" ), false, false )
+		end
+	end
+)
---
+++ resources/account/cegui/server/selection.lua
@@ -0,0 +1,10 @@
+﻿addEvent( getResourceName( resource ) .. ":cegui:characters", true )
+addEventHandler( getResourceName( resource ) .. ":cegui:characters", root,
+	function( )
+		if ( client ~= source ) or ( not getElementData( client, "client:id" ) ) then return end
+		local result, num_rows = exports.database:query( "SELECT `name`, DATEDIFF(`last_login`, NOW()) AS `last_login` FROM `characters` WHERE `account_id` = ?", getElementData( client, "client:id" ) )
+		if ( result ) and ( num_rows > 0 ) then
+			triggerClientEvent( client, getResourceName( resource ) .. ":cegui:characters", client, result )
+		end
+	end
+)
